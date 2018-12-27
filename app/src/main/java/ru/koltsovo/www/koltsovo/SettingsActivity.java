503c51d070fd2df8e506680d6aec43a10784197f
Sergey Uharskiy <uharskiy@gmail.com>
2018-12-27 11:46:12 +0500

    Версия 1.2.12 - Логирование исключений

--- app/src/main/java/ru/koltsovo/www/koltsovo/SettingsActivity.java
+++ app/src/main/java/ru/koltsovo/www/koltsovo/SettingsActivity.java
@@ -2,7 +2,6 @@ package ru.koltsovo.www.koltsovo;
 
 import android.annotation.SuppressLint;
 import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -23,6 +22,7 @@ import android.widget.Toast;
 import com.anjlab.android.iab.v3.BillingProcessor;
 import com.anjlab.android.iab.v3.SkuDetails;
 import com.anjlab.android.iab.v3.TransactionDetails;
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
@@ -49,11 +49,12 @@ public class SettingsActivity extends AppCompatActivity implements BillingProces
     @Override
     @SuppressWarnings("ConstantConditions")
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        settings = getSharedPreferences(Constants.APP_PREFERENCES, Context.MODE_PRIVATE);
+        settings = getSharedPreferences(Constants.APP_PREFERENCES, MODE_PRIVATE);
         int appTheme = settings.getInt(Constants.APP_PREFERENCES_APP_THEME, APP_THEME);
         setTheme(appTheme);
 
         super.onCreate(savedInstanceState);
+
         setContentView(LAYOUT);
 
         // Google Analytics
@@ -151,7 +152,9 @@ public class SettingsActivity extends AppCompatActivity implements BillingProces
     }
 
     @Override
-    public void onBillingError(int errorCode, Throwable error) {}
+    public void onBillingError(int errorCode, Throwable error) {
+        Crashlytics.logException(error);
+    }
 
     @Override
     public void onPurchaseHistoryRestored() {}
@@ -203,7 +206,7 @@ public class SettingsActivity extends AppCompatActivity implements BillingProces
                 String textPrice = price + " " + currency;
                 final String buttonText = getString(R.string.button_ads_disable) + " " + textPrice;
 
-                if (!settings.getString(Constants.APP_PREFERENCES_ADS_DISABLE_PRICE, "").equals(textPrice)) {
+                if (!textPrice.equals(settings.getString(Constants.APP_PREFERENCES_ADS_DISABLE_PRICE, ""))) {
                     SharedPreferences.Editor editor = settings.edit();
                     editor.putString(Constants.APP_PREFERENCES_ADS_DISABLE_PRICE, textPrice);
                     editor.apply();
@@ -274,6 +277,7 @@ public class SettingsActivity extends AppCompatActivity implements BillingProces
         try {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + getPackageName())));
         } catch (ActivityNotFoundException e) {
+            Crashlytics.logException(e);
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName())));
         }
     }
